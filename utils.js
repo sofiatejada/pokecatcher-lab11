f95bc97299f47886c0a3fef551e862e76a3bc9d4
Sofia Tejada <sofianais@icloud.com>
2021-05-17 15:37:06 -0700

    create pokedex array and findById function

---
+++ utils.js
@@ -0,0 +1,7 @@
+export function findById(pokedexArray, selectionId) {
+    for (let pokemon of pokedexArray) {
+        if (pokemon.id === selectionId) {
+            return pokemon;
+        } else return null;
+    }
+}
