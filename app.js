65682f95795f716cf7031a850ac218257a0a2a9f
Sofia Tejada <sofianais@icloud.com>
2021-05-17 19:34:35 -0700

    populate app.js file

--- app.js
+++ app.js
@@ -1,8 +1,58 @@
 // import functions and grab DOM elements
+import { getPokedex, setPokedex, capturePokemon, encounterPokemon } from './render-poke-functions.js';
+import { pokedex } from './pokedex.js';
 
-// initialize state
+const poke1 = document.querySelector('#poke-radio-1');
+const poke2 = document.querySelector('#poke-radio-2');
+const poke3 = document.querySelector('#poke-radio-3');
+const pokeImage1 = document.querySelector('#poke-image-1');
+const pokeImage2 = document.querySelector('#poke-image-2');
+const pokeImage3 = document.querySelector('#poke-image-3');
+const button = document.querySelector('button');
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+// on LOAD
+
+renderPokemon();
+
+function getRandomPokemon() {
+    const randomNumber = Math.floor(Math.random() * pokedex.length);
+
+    const randomPokemon = pokedex.id[randomNumber];
+
+    return randomPokemon;
+}
+
+
+function renderPokemon() {
+
+  
+    let pokemon1 = getRandomPokemon();
+    let pokemon2 = getRandomPokemon();
+    let pokemon3 = getRandomPokemon();
+
+    while (pokemon1.id === pokemon2.id || pokemon2.id === pokemon3.id || pokemon1.id === pokemon3.id) {
+        pokemon1 = getRandomPokemon();
+        pokemon2 = getRandomPokemon();
+        pokemon3 = getRandomPokemon();
+    }
+
+    encounterPokemon(pokemon1);
+    encounterPokemon(pokemon2);
+    encounterPokemon(pokemon3);
+
+    pokeImage1.src = pokemon1.image;
+    pokeImage2.src = pokemon2.image;
+    pokeImage3.src = pokemon3.image;
+
+    poke1.value = pokemon1.id;
+    poke2.value = pokemon2.id;
+    poke3.value = pokemon3.id;
+  
+  
+}
+
+button.addEventListener('click', () => {
+
+    
+
+});
